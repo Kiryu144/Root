938e7e6269b8bd1ba7eecec35702e6ef7642f611
Kiryu144 <123davidLP@gmail.com>
2017-05-31 23:18:52 +0200

    *Last commit before the actual game

--- source/main.cpp
+++ source/main.cpp
@@ -3,47 +3,75 @@
 #include <GLM/GTX/string_cast.hpp>
 
 int main() {
-    /* Initiates GLFW */
-    AM::WindowHandler::init();
+    try {
+        /* Initiates GLFW */
+        AM::WindowHandler::init();
 
-    /* Creates window and openGL context. Without it, the application will crash! */
-    AM::WindowHandler::createWindow("Amethyst rocks!", glm::vec2(1440, 900), glm::vec4(0.5f, 0.25f, 0.25f, 1));
+        /* Creates window and openGL context. Without it, the application will crash! */
+        AM::WindowHandler::createWindow("Amethyst rocks!", glm::vec2(1440, 900), glm::vec4(0, 0, 0, 255));
+        AM::InputController::lockMousePosition(glm::vec2(1440/2.0f, 900/2.0f));
 
-    AM::Camera camera(AM::Transformation(AM::Position(-2, 0, 0), AM::Quaternion(), AM::Scale(1440, 900, 900)));
+        AM::Camera camera(AM::Transformation(AM::Position(-2, 0, 0), AM::Quaternion(0, 20, 0), AM::Scale(1440, 900, 900)));
+        AM::Debugcamera debugcamera(&camera);
 
-    AM::Shader barrelShader("./texture_diffuse.vert", "./texture_diffuse.frag");
+        AM::Shader barrelShader("./texture_diffuse.vert", "./texture_diffuse.frag");
+        AM::Texture barrelTexture("./Texture.png");
 
-    AM::Transformation barrelTransformation;
+        AM::Transformation barrelTransformation(AM::Position(glm::vec3(0, 0, 0)), AM::Quaternion(), AM::Scale(glm::vec3(20, 20, 20)));
 
-    AM::WavefrontData barrelMesh;
-    AM::WavefrontParser::readObj("./Barrel.obj", barrelMesh);
+        AM::WavefrontData barrelMesh;
+        AM::WavefrontParser::readObj("./nudemuch.obj", barrelMesh);
 
-    AM::VBO<glm::vec3, 3> barrelVertices;
-    AM::VBO<glm::vec2, 3> barrelUVs;
-    AM::VBO<glm::vec3, 3> barrelNormals;
+        AM::VBO<glm::vec3, 3> barrelVertices;
+        AM::VBO<glm::vec2, 3> barrelUVs;
+        AM::VBO<glm::vec3, 3> barrelNormals;
 
-    barrelVertices.upload(barrelMesh.vertexData.get());
-    barrelUVs.upload(barrelMesh.uvData.get());
-    barrelNormals.upload(barrelMesh.normalData.get());
+        barrelVertices.upload(barrelMesh.vertexData.get());
+        barrelUVs.upload(barrelMesh.uvData.get());
+        barrelNormals.upload(barrelMesh.normalData.get());
 
-    do {
-        camera.update();
+        do {
+            AM::DeltaTime::update();
+            camera.update();
+            debugcamera.updateMovement();
 
-        barrelShader.bind();
-        barrelShader.setUniform("projection", camera.getProjection());
-        barrelShader.setUniform("view", camera.getView());
-        barrelShader.setUniform("transformation", barrelTransformation.getMatrix());
-        barrelShader.setUniform("lightPos", glm::vec3(sin(glfwGetTime()), 0, cos(glfwGetTime())));
-        barrelShader.setUniform("lightColor", glm::vec3(1, 1, 1));
+            barrelShader.bind();
+            barrelShader.setUniform("projection", camera.getProjection());
+            barrelShader.setUniform("view", camera.getView());
+            barrelShader.setUniform("transformation", barrelTransformation.getMatrix());
+            barrelShader.setUniform("pointLight", camera.getTransformation().getPosition().get());
 
-        barrelVertices.bind(0);
-        barrelUVs.bind(1);
-        barrelNormals.bind(2);
+            barrelVertices.bind(0);
+            barrelUVs.bind(1);
+            barrelNormals.bind(2);
 
-        glDrawArrays(GL_TRIANGLES, 0, barrelMesh.vertexData.size());
+            glDrawArrays(GL_TRIANGLES, 0, barrelMesh.vertexData.size());
 
-    } while(AM::WindowHandler::poll()); /* Handles OS events. Returns false if closing is requested */
+            AM::InputController::process();
+        } while (AM::WindowHandler::poll()); /* Handles OS events. Returns false if closing is requested */
 
+    }catch(__AmethystException exception){
+        AM::Logger::error(exception.getExceptionError(), 2);
+    }
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
